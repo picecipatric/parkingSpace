5949aa7a88adc1bddc50c1861e665328b60fd2a0
99Jannis <72573490+99Jannis@users.noreply.github.com>
2024-11-10 17:31:11 +0100

    added plot function and Canny edge detection - added function to plot multiple images - added Canny edge detection

--- main.py
+++ main.py
@@ -4,20 +4,44 @@ import numpy as np
 import csv
 import math
 
-
-# url = "jenfjnw.oei"
-filename = "images/parkingLot.jpeg"
-
-# Bild einlesen mit Farben
-img = cv2.imread(filename, cv2.IMREAD_COLOR)
-
-# Überprüfen, ob das Bild erfolgreich eingelesen wurde
-if img is None:
-    print("Error: Could not read image file", filename)
-    sys.exit(1)
-
-cv2.imshow("Image", img)
+from src.display_images import ImagePlotter
 
 
 
-cv2.waitKey(0)
+if __name__ == "__main__": 
+    ip = ImagePlotter()
+    # url = "jenfjnw.oei"
+    filename = R"images/parkingLot.jpeg"
+
+    # Input image
+    try: 
+        img = cv2.imread(filename, cv2.IMREAD_COLOR)
+    except: 
+        print("Error: Could not read image file", filename)
+        sys.exit()
+    ip.add_image_to_plot("input", img)
+    
+    # grayscale image
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ip.add_image_to_plot("grayscale", img_gray)
+    
+    # blur image
+    img_blur = cv2.GaussianBlur(img_gray, (5,5), cv2.BORDER_DEFAULT)
+    ip.add_image_to_plot("blur", img_blur)
+    
+    # canny edge detection
+    low_threshold = 15
+    ratio = 3
+    kernel_size = 5
+    detected_edges = cv2.Canny(img_blur, low_threshold, low_threshold*ratio, kernel_size)
+    mask = detected_edges != 0
+    img_canny = img_gray * (mask[:].astype(img.dtype))
+    ip.add_image_to_plot("canny", img_canny)
+    
+    # plot/display the images
+    ip.plot_images()
+    
+
+
+
+
