01d211215e0f51459220f33cee95bba716f74a0d
PatrickLutz11 <163037264+PatrickLutz11@users.noreply.github.com>
2024-12-14 14:27:07 +0100

    Pixel_recognition

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 from src.util import get_parking_lines, availability
 
 
@@ -22,20 +23,30 @@ if __name__ == "__main__":
     
     status = [None for j in parking_lots]
     
-    #print(parking_lots[0])
     frame_number = 0
+    papap = 1
     at_frame = 50
     ret = True
     while ret:
         ret,frame = video.read()
+        videogray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        videoblur = cv2.GaussianBlur(videogray, (3, 3),1)
+        videothresh = cv2.adaptiveThreshold(
+            videoblur, 255,
+            cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+            cv2.THRESH_BINARY_INV, 25, 16)
+        
+        videoMedian = cv2.medianBlur(videothresh, 5)
+        
+        kernel = np.ones((3,3), np.uint8)
+        videoDilate = cv2.dilate(videoMedian,kernel, iterations=1)
+        
         
         if frame_number % at_frame == 0:
             for parking_lot_index, parking_lot in enumerate(parking_lots):
-                x1, y1, w, h, = parking_lot
-
-                parking_lot_check = frame[y1:y1 + h, x1:x1 + w, :]
-
-                parking_status = availability(parking_lot_check)
+                x1, y1, w, h = parking_lot
+                
+                parking_status = availability(parking_lot,videoDilate)
                 
                 status[parking_lot_index] = parking_status
         
@@ -43,10 +54,11 @@ if __name__ == "__main__":
             parking_status = status[parking_lot_index]
             x1, y1, w, h, = parking_lots[parking_lot_index]
             
+            
             if parking_status:
-                frame = cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 0, 255), 2)
-            else:
                 frame = cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 2)
+            else:
+                frame = cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 0, 255), 2)
         
         cv2.imshow('frame',frame)
         if cv2.waitKey(25) & 0xFF == ord('q'):
